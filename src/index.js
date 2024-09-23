28d97a415c5403657d446dbba52216a4f2489c03
AndraTodor <andra_t27@yahoo.com>
2024-09-23 21:42:56 +0300

    inlocuire BrowserRouter cu HashRouter
    pentru proiectele de pe Github - se evita deschidera paginii 404 daca se foloseste HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // schimb BrowserRouter cu HashRouter pentru a evita deschiderea paginii de eroare 404
 import App from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
